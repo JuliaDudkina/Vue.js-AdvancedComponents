511c9f44ef8c5cf4832bcb2c3386a70d8c5529bf
juliadud <yuliiadudkina@gmail.com>
2021-11-04 18:58:49 +0200

    adding base card template

---
+++ src/components/BaseCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <!-- to not render a certain el if it does not have content-->
+    <header v-if="$slots.header">
+      <slot name="header">
+<!--        <h2>The Default</h2>-->
+      </slot>
+    </header>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseCard"
+}
+</script>
+
+<style scoped>
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  div {
+    margin: 2rem auto;
+    max-width: 30rem;
+    border-radius: 12px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    padding: 1rem;
+  }
+</style>
